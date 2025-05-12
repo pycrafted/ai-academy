8ec98d227a921241c110be853c1b897a6a79e3bc
pycrafted <abdoulayelah@esp.sn>
2025-05-12 19:46:49 +0000

    affichage final

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { useNavigate } from 'react-router-dom';
 import CourseList from '../components/CourseList';
 import Counter from '../components/Counter';
+import './HomePage.css';
 
 const HomePage = ({ courses }) => {
   const navigate = useNavigate();
